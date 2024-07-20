2a67d5f498acfe1b17afd84b4a62b3f7f6091368
sriram32005 <sriram32005@gmail.com>
2024-07-21 00:37:01 +0530

    week1

---
+++ Day1-16-07-24/Comments.java
@@ -0,0 +1,27 @@
+public class Comments {
+    public static void main(String[] args) {
+        final int b = 12;
+        int a=25;
+        a+=10;
+
+        //b+=5; This is a single line comment (final variable cannot be assigned a value)
+
+        /* This is a
+        multiline
+        comment*/
+
+        //comments will not be executed
+
+        String str= "Hello world";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(str);
+    }
+}
+
+
+
+
+
+
+
---
+++ Day1-16-07-24/Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+    public static void main(String[] args) {
+        final int b = 12;
+        int a=25;
+        a+=10;
+        //b+=5;
+        String str= "Hello world";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(str);
+    }
+}
+
+
+
+
+
