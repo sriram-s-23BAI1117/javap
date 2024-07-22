cb810048e0a8bac3d7a4d93049038fad177cd630
sriram32005 <sriram32005@gmail.com>
2024-07-22 11:04:36 +0530

    theory-1

---
+++ Theory-22-07-24/PlusOperator.java
@@ -0,0 +1,10 @@
+public class PlusOperator{
+    public static void main(String[] args){
+        String firstname = "John";
+        String lastname = "Dow";
+        System.out.println(firstname + " " + lastname);
+    }
+}
+
+
+
