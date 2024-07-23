7f847b2ee01300e7b636d5af2d8c48fb7faf52bb
sriram32005 <sriram32005@gmail.com>
2024-07-23 12:43:42 +0530

    Day-2-mid

---
+++ Theory-2-23-07-24/Comp.java
@@ -0,0 +1,12 @@
+public class Comp {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 9;
+        System.out.println(x>y);
+        System.out.println(10==15);
+    }
+}
+
+
+
+
---
+++ Theory-2-23-07-24/If.java
@@ -0,0 +1,12 @@
+public class If {
+    public static void main(String[] args) {
+        if (20 > 18){
+            System.out.println("20 is greater than 18");
+        }
+    }
+}
+
+
+
+
+
