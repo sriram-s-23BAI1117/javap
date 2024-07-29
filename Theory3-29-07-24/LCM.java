aa26d6343f7db78340374234a209ee157fe6f3de
sriram32005 <sriram32005@gmail.com>
2024-07-29 18:22:24 +0530

    Day -3 theory over

--- Theory3-29-07-24/LCM.java
+++ Theory3-29-07-24/LCM.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class LCM {
     public static void main(String[] args) {
-        int n1,n2;
-        
+        Scanner input = new Scanner(System.in);
+        int n1 = input.nextInt();
+        int n2 = input.nextInt();
+        int num = (n1>n2)? n1:n2;
+        while (true){
+            if (num%n1==0 && num%n2==0){
+                break;
+            }
+            num++;
+        }
+        input.close();
+        System.out.printf("The LCM of %d and %d is %d",n1,n2,num);
     }
 }
